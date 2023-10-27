8a7a1a05ae440f5c046895684a193fd302c484ce
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2023-10-27 14:32:37 +0700

    Membuat Text Register

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viewmodel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -32,7 +34,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -70,6 +74,12 @@ fun TampilLayout(modifier: Modifier = Modifier) {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(20.dp))
         {
+            Text(
+                text = "Register",
+                fontSize = 30.sp,
+                color = Color.White
+            )
+
             TampilForm()
         }
 
@@ -214,7 +224,15 @@ fun SelectJK (
                     }
                 ),
                 verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ){
+                RadioButton(
+                    selected = selectedValue == item,
+                    onClick = {
+                        selectedValue = item
+                        onSelectionChanged(item)
+                    }
+                )
                 RadioButton(
                     selected = selectedValue == item,
                     onClick = {
